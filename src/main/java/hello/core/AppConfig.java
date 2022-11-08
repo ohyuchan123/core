ff5d459b476f3e1995dee4112198e13ce825fe07
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-11-09 01:06:23 +0900

    AppConfig 리펙터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,9 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrederServiceImpl;
 
@@ -17,5 +16,13 @@ public class AppConfig {
                 new MemoryMemberRepository(),
                 new FixDiscountPolicy()
         );
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
